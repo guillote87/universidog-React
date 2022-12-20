db075fad208857097c5a01ecac568235556f3a02
guillote87 <guillermo27@gmail.com>
2022-12-20 16:41:32 -0300

    universi-ghpages

--- src/components/Formacion/Formacion.js
+++ src/components/Formacion/Formacion.js
@@ -3,16 +3,16 @@ import './Formacion.css'
 
 export const Formacion = () => {
   return (
-    <div class="formacion">
-            <div class="container formacion-container">
-                <div class="formacion-text">
-                    <h2 class="subtitle">Curso de formacion para cachorros</h2>
-                    <p class="formacion-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui omnis
+    <div className="formacion">
+            <div className="container formacion-container">
+                <div className="formacion-text">
+                    <h2 className="subtitle">Curso de formacion para cachorros</h2>
+                    <p className="formacion-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui omnis
                         cupiditate aspernatur praesentium.</p>
-                    <a href="#" class="cta">Inscribirme</a>
+                    <a href="#" className="cta">Inscribirme</a>
                 </div>
-                <figure class="formacion-card">
-                    <img src="./images/formacion.jpg" class="formacion-image"/>
+                <figure className="formacion-card">
+                    <img src="./images/formacion.jpg" className="formacion-image"/>
                 </figure>
 
             </div>
